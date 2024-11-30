9c731ce55f0b5403ec2be6f918510b8644dda803
maleehamir <maleehamir009@gmil.com>
2024-11-30 14:50:50 +0500

    ininsial commit

---
+++ counter app.js
@@ -0,0 +1,16 @@
+function onload(){
+    var seconds = 0;
+    var button_increment = document.getElementById('button-increment');
+    var button_decrement = document.getElementById('button-decrement');
+    var button_reset = document.getElementById('button-reset')
+}
+button_increment.onclick = function(){
+     increase(increase);
+}
+button_decrement.onclick = function(){
+    decrease(decrease);
+}
+button_reset.onclick = function(){
+    reset(reset);
+}
+var button_increment = document.getElementById("button_increment");
